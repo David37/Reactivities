d70e4e3aabb3f158c2a7bb4b91ee886d081d823b
David37 <davidkondic@protonmail.com>
2021-03-09 20:02:32 +0200

    Initial commit- WebAPI details

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //path api/activities/
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")] // path /api/activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}

8f04f9bac2467dc52a3738361b89135542cb6cba
David37 <davidkondic@protonmail.com>
2021-03-17 18:20:04 +0200

    Added front-end CRUD functionalties

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,39 @@
+import { Grid} from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+import ActivityDetails from '../Details/ActivityDetails'
+import ActivityForm from '../form/ActivityForm'
+import ActivityList from './ActivityList'
+
+interface Props{
+    activities:Activity[];
+    selectedActivity:Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    handleActivitySubmission: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+}
+const ActivityDashboard = ({activities, selectedActivity, selectActivity, cancelActivity, editMode, openForm, closeForm, handleActivitySubmission, deleteActivity}:Props) => {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+            </Grid.Column>
+            <Grid.Column width ='6'>
+                {selectedActivity && !editMode && 
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelActivity={cancelActivity} 
+                    openForm={openForm}/>
+                }
+                {editMode &&<ActivityForm closeForm={closeForm} activity={selectedActivity} handleActivitySubmission={handleActivitySubmission}/>}
+                
+            </Grid.Column>
+            
+        </Grid>
+    )
+}
+
+export default ActivityDashboard
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, useState } from 'react'
+import { Button, Form, Segment } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+
+interface Props{
+    closeForm: () => void;
+    activity:Activity | undefined;
+    handleActivitySubmission: (activity: Activity) => void;
+}
+const ActivityForm = ({closeForm, activity: selectedActivity, handleActivitySubmission}:Props) => {
+    
+    const InitialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        date: '',
+        description: '',
+        category: '',
+        city: '',
+        venue: ''
+    }
+
+    const [activity, setActivity]= useState(InitialState);
+
+    function handleSubmit(){
+        handleActivitySubmission(activity);
+    }
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
+        const {name,value}= event.target;
+        setActivity({...activity,[name]:value})
+    }
+
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <Form.Input placeholder= 'title' value={activity.title} name='Title' onChange={handleInputChange} />
+                <Form.TextArea placeholder= 'description' value={activity.description} name='description' nChange={handleInputChange}/>
+                <Form.Input placeholder= 'category' value={activity.category} name= 'category' onChange={handleInputChange}/>
+                <Form.Input placeholder= 'date' value={activity.date} name= 'date' onChange={handleInputChange}/>
+                <Form.Input placeholder= 'city' value={activity.city} name= 'city' onChange={handleInputChange}/>
+                <Form.Input placeholder= 'venue' value={activity.venue} name= 'venue' onChange={handleInputChange}/>
+                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button onClick={closeForm} floated= 'right' type='button' content='Cancel'/>
+            </Form>
+        </Segment>
+    )
+}
+
+export default ActivityForm

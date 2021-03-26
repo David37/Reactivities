994c26b67d8435eae1a5a46eec9430c692f90897
David37 <davidkondic@protonmail.com>
2021-03-26 14:43:12 +0200

    refactored create and update activity components

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -13,25 +13,17 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
-import { Activity } from '../../../app/models/activity';
+import { ActivityFormValues } from '../../../app/models/activity';
 
 
 const ActivityForm = () => {
     const {activityStore} = useStore();
-    const {loading, updateActivity, createActivity, loadActivity, loadingInitial}= activityStore;
+    const { updateActivity, createActivity, loadActivity, loadingInitial}= activityStore;
     const {id} = useParams<{id: string}>();
     const history = useHistory();
 
  
-    const [activity, setActivity]= useState<Activity>({
-        id: '',
-        title: '', 
-        date: null,
-        description: '',
-        category: '',
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity]= useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title:Yup.string().required('The activity title is required'),
@@ -44,11 +36,11 @@ const ActivityForm = () => {
     })
 
     useEffect(()=>{
-        if(id) loadActivity(id).then(activity => setActivity(activity!))
+        if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     },[id, loadActivity]);
 
-    function handleFormSubmit(activity:Activity){
-        if(activity.id.length === 0) {
+    function handleFormSubmit(activity:ActivityFormValues){
+        if(!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
@@ -88,7 +80,7 @@ const ActivityForm = () => {
                         <MyTextInput placeholder= 'venue' name= 'venue'/>
                         <Button 
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading= {loading} 
+                            loading= {isSubmitting} 
                             floated='right' 
                             positive type='submit' 
                             content='Submit'
